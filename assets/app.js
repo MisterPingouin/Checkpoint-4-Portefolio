f72f4b91ca2d8d807acf78dfae9a4cc3cb7857ce
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-07-18 14:19:36 +0200

    test gallery

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,28 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+const buttons = document.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(document.querySelectorAll('[role="tabpanel"]'));
+
+function toggleAttribute(elements, attribute, value) {
+    elements.forEach((element) => {
+        element.setAttribute(attribute, value);
+    });
+}
+
+function showTabContent(e) {
+    const id = this.id;
+
+    toggleAttribute(tabPanels, "hidden", true);
+    toggleAttribute(buttons, "aria-selected", false);
+    toggleAttribute([this], "aria-selected", true);
+    const tabPanel = tabPanels.find(
+        (panel) => panel.getAttribute("aria-labelledby") === id
+    );
+    tabPanel.hidden = false;
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", showTabContent);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        fontFamily: {
+            'titlefont': ['Montserrat'],
+            'subtitlefont': ['Lora'],
+            'contentfont': ['Hind Madurai'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
