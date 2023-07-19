25b6cc0b3006ec05019ff0bf6d8bc4429dc9fa3f
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-07-19 09:58:04 +0200

    Presentation Project V0.4

--- assets/presentation.js
+++ assets/presentation.js
@@ -1,22 +1,24 @@
 import './styles/presentation.scss';
+
 document.addEventListener("DOMContentLoaded", function() {
     var articles = document.querySelectorAll('article');
     var windowHeight = window.innerHeight;
-  
+
     function checkPosition() {
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            var positionFromTop = articles[i].getBoundingClientRect().top;
-  
-            if (positionFromTop - windowHeight <= 0) {
+            var positionFromBottom = articles[i].getBoundingClientRect().bottom;
+
+            if (positionFromBottom - windowHeight <= 0) {
                 article.classList.add('current');
             } else {
                 article.classList.remove('current');
             }
         }
     }
-  
+
     window.addEventListener('scroll', checkPosition);
     window.addEventListener('resize', checkPosition);
+    // Exécutez la fonction une fois au chargement pour initialiser correctement les classes
+    checkPosition();
 });
-  
